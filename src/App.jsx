952ed84a765a028d0eca09094da58554cc629fd5
zameer <zameer.sheikh@fissionlabs.com>
2023-02-18 22:13:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Header from "remote/Header";
+import Footer from "remote/Footer";
+
+import "./index.scss";
+
+const App = () => (
+  <div>
+    <Header />
+    <main>
+      <h3>Host1</h3>
+    </main>
+    <Footer />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
